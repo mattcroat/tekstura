335080e81f40274e6c6647109b25392586772a06
mattcroat <mattcroat.dev@gmail.com>
2021-02-01 22:59:19 +0100

    feat: Add Google fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,15 @@ class MyDocument extends Document {
       <Html lang="en">
         <Head>
           <link rel="icon" href="/favicon.ico" />
+          <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Merriweather:wght@300;700&display=swap"
+            rel="stylesheet"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@700&display=swap"
+            rel="stylesheet"
+          />
         </Head>
         <body>
           <Main />
