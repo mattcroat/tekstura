f06251e6743f5fa03445517f35dde2e9f1e503ea
mattcroat <mattcroat.dev@gmail.com>
2021-02-02 23:17:10 +0100

    wip: Landing page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,49 +1,71 @@
 import styled from '@emotion/styled'
 import Link from 'next/link'
 
-const Landing = styled.main`
-  height: 100vh;
-  display: grid;
-  grid-template-rows: repeat(2, 1fr);
-  grid-template-areas:
-    'top-section'
-    'bottom-section';
+const Layout = styled.div`
+  @media screen and (min-width: 500px) {
+    position: relative;
+    height: 100vh;
+    display: grid;
+    grid-template-rows: auto 1fr;
+    grid-template-areas:
+      'header'
+      'main';
+  }
 `
 
-const TopSection = styled.section`
-  grid-area: top-section;
+const Main = styled.main`
+  @media screen and (min-width: 500px) {
+    display: grid;
+    grid-template-rows: 1fr 50vh;
+    grid-template-areas:
+      'byline'
+      'recipe';
+  }
 `
 
 const Header = styled.header`
-  display: flex;
-  justify-content: space-between;
-  align-items: baseline;
-  margin: var(--spacing-32) var(--spacing-64);
+  margin: var(--spacing-24);
 
-  ul {
+  nav {
+    display: none;
+  }
+
+  @media screen and (min-width: 500px) {
+    grid-area: header;
     display: flex;
-    gap: var(--spacing-32);
-    list-style: none;
-    font-weight: 700;
+    justify-content: space-between;
+    align-items: baseline;
+    margin: var(--spacing-32) var(--spacing-64) var(--spacing-32)
+      var(--spacing-64);
+
+    nav {
+      display: block;
+    }
+
+    ul {
+      display: flex;
+      gap: var(--spacing-32);
+      list-style: none;
+      font-weight: 700;
+    }
   }
 `
 
 const Byline = styled.div`
-  margin-top: var(--spacing-64);
-  text-align: center;
+  margin: var(--spacing-64) var(--spacing-24) var(--spacing-24)
+    var(--spacing-24);
+
+  @media screen and (min-width: 500px) {
+    grid-area: byline;
+    margin: var(--spacing-64) 0;
+    text-align: center;
+  }
 `
 
 const Newsletter = styled.section`
-  position: absolute;
-  z-index: 1;
-  width: 520px;
-  display: grid;
-  place-self: center;
-  gap: var(--spacing-24);
   padding: var(--spacing-24);
+  margin: var(--spacing-24) 0;
   background-color: var(--color-secondary-bg);
-  box-shadow: var(--shadow-lg);
-  border-radius: var(--radius-base);
   text-align: center;
 
   img {
@@ -54,37 +76,57 @@ const Newsletter = styled.section`
     margin: 0 auto;
   }
 
-  h2 {
-    color: var(--color-gold);
+  h3 {
+    margin: var(--spacing-16) 0;
   }
 
-  h3 {
-    margin-bottom: var(--spacing-8);
+  fieldset {
+    display: inline-block;
+    margin-top: var(--spacing-8);
+    border: 1px solid var(--color-input-border);
+    border-radius: var(--radius-base);
   }
 
   input {
-    padding: var(--spacing-8);
+    padding: 0 var(--spacing-8);
+    font-size: inherit;
     color: var(--color-input-text);
-    background-color: var(--color-input-bg);
-    border: 1px solid var(--color-input-border);
+    background-color: var(--color-secondary-bg);
+    border: none;
+  }
+
+  button {
+    padding: var(--spacing-8);
+    font-family: inherit;
+    font-weight: 700;
+    font-size: inherit;
+    background-color: var(--color-gold);
+    border: none;
+    border-left: 1px solid var(--color-input-border);
+  }
+
+  @media screen and (min-width: 500px) {
+    z-index: 1;
+    position: absolute;
+    top: 50%;
+    right: 50%;
+    transform: translate(50%, -50%);
+    margin: 0;
+    box-shadow: var(--shadow-lg);
     border-radius: var(--radius-base);
   }
 `
 
-const BottomSection = styled.section`
-  grid-area: bottom-section;
-  position: relative;
-  height: 100%;
-  color: var(--color-text-light);
-`
-
 const Recipe = styled.div`
   position: relative;
-  height: 100%;
+  height: 400px;
   display: grid;
   gap: var(--spacing-24);
   place-content: center;
   place-items: center;
+  padding: 0 var(--spacing-24);
+  color: var(--color-text-light);
+  text-align: center;
 
   &::before {
     content: '';
@@ -106,6 +148,15 @@ const Recipe = styled.div`
     object-fit: cover;
   }
 
+  h2 {
+    color: var(--color-gold);
+  }
+
+  h3 {
+    color: var(--color-bg-secondary);
+    text-transform: lowercase;
+  }
+
   a::after {
     content: '';
     display: block;
@@ -118,56 +169,63 @@ const Recipe = styled.div`
   a:hover::after {
     width: 0;
   }
+
+  @media screen and (min-width: 500px) {
+    grid-area: recipe;
+    height: 100%;
+    padding: 0;
+  }
 `
 
 export function Home() {
   return (
-    <Landing>
-      <TopSection>
-        <Header>
-          <div>
-            <h1>Cuisine</h1>
-          </div>
-          <nav>
-            <ul>
-              <li>
-                <Link href="/">
-                  <a>Home</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/recipes">
-                  <a>Recipes</a>
-                </Link>
-              </li>
-              <li>
-                <Link href="/about">
-                  <a>About</a>
-                </Link>
-              </li>
-            </ul>
-          </nav>
-        </Header>
+    <Layout>
+      <Header>
+        <div>
+          <h1>
+            <Link href="/">
+              <a>Cuisine</a>
+            </Link>
+          </h1>
+        </div>
+        <nav>
+          <ul>
+            <li>
+              <Link href="/">
+                <a>Home</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/recipes">
+                <a>Recipes</a>
+              </Link>
+            </li>
+            <li>
+              <Link href="/about">
+                <a>About</a>
+              </Link>
+            </li>
+          </ul>
+        </nav>
+      </Header>
 
+      <Main>
         <Byline>
-          <div>
-            <h1>Welcome to Cuisine!</h1>
-            <p>I enjoy sharing how to make great food with others.</p>
-          </div>
+          <h1>Welcome to Cuisine!</h1>
+          <h3>I enjoy sharing how to make great food with others.</h3>
         </Byline>
-      </TopSection>
 
-      <Newsletter>
-        <img src="/images/avatar.webp" alt="Avatar" />
-        <h2>Like what I do?</h2>
-        <form>
-          <h3>Subscribe to get updates:</h3>
-          <label htmlFor="newsletter">Email: </label>
-          <input type="text" id="newsletter" placeholder="mail@mail.com" />
-        </form>
-      </Newsletter>
+        <Newsletter>
+          <img src="/images/avatar.webp" alt="Avatar" />
+          <h3>Get updates in your inbox</h3>
+          <form>
+            <fieldset>
+              <input type="text" placeholder="mail@mail.com" />
+              <button type="button">Subscribe</button>
+            </fieldset>
+          </form>
+        </Newsletter>
 
-      <BottomSection>
         <Recipe>
           <img src="/images/dish.webp" alt="Dish" />
           <h2>You never had eggs like these</h2>
@@ -177,7 +235,7 @@ export function Home() {
             </Link>
           </h3>
         </Recipe>
-      </BottomSection>
-    </Landing>
+      </Main>
+    </Layout>
   )
 }
