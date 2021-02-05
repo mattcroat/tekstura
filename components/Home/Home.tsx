78ef0bdb0205983025e298c541622579df8549ba
mattcroat <mattcroat.dev@gmail.com>
2021-02-05 21:26:43 +0100

    wip: Landing page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -1,7 +1,29 @@
+import React from 'react'
 import styled from '@emotion/styled'
+import { AnimatePresence, motion } from 'framer-motion'
 import Link from 'next/link'
 
 import { screenSize } from '@/root/styles/mediaQueries'
+
+type HeaderMobile = {
+  isMenuOpen: boolean
+}
+
+const menu = {
+  hidden: { opacity: 0, height: 0 },
+  show: {
+    opacity: 1,
+    height: 'auto',
+    transition: {
+      staggerChildren: 0.1,
+    },
+  },
+}
+
+const menuItem = {
+  hidden: { opacity: 0 },
+  show: { opacity: 1 },
+}
 
 const Layout = styled.div`
   ${screenSize.sm} {
@@ -26,13 +48,8 @@ const Layout = styled.div`
 `
 
 const Header = styled.header`
-  height: 100px;
-  padding: var(--spacing-24);
+  display: none;
   z-index: 2;
-
-  nav {
-    display: none;
-  }
 
   ${screenSize.sm} {
     display: flex;
@@ -44,21 +61,52 @@ const Header = styled.header`
     ul {
       display: flex;
       gap: var(--spacing-32);
-      list-style: none;
       font-weight: 700;
-    }
-  }
-
-  ${screenSize.md} {
-    nav {
-      display: block;
     }
   }
 `
 
+const ToggleMenuIcon = styled.button`
+  height: 28px;
+  width: 28px;
+  background: none;
+  border: none;
+  cursor: pointer;
+
+  ${screenSize.sm} {
+    display: none;
+  }
+`
+
+const HeaderMobile = styled.header<HeaderMobile>`
+  margin-bottom: var(--spacing-32);
+  padding: var(--spacing-24) var(--spacing-24) 0 var(--spacing-24);
+  background-color: ${({ isMenuOpen }) =>
+    isMenuOpen && 'var(--color-highlight)'};
+  transition: background-color 0.3s;
+  z-index: 2;
+
+  ${screenSize.sm} {
+    display: none;
+  }
+`
+
+const Menu = styled(motion.nav)`
+  padding: 24px 0;
+  font-weight: 700;
+
+  ul {
+    display: flex;
+    flex-direction: column;
+    gap: var(--spacing-8);
+  }
+
+  a {
+    display: inline-block;
+  }
+`
+
 const Main = styled.main`
-  /* take heading into consideration */
-  height: calc(100vh - 100px);
   display: grid;
   grid-template-rows: auto 1fr 1fr;
   gap: var(--spacing-24);
@@ -135,6 +183,7 @@ const Newsletter = styled.section`
     background-color: var(--color-highlight);
     border: none;
     border-left: 1px solid var(--color-input-border);
+    cursor: pointer;
   }
 
   ${screenSize.sm} {
@@ -275,13 +324,20 @@ const Cover = styled.section`
   }
 `
 
+const Flex = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: baseline;
+`
+
 export function Home() {
+  const [isOpen, setIsOpen] = React.useState(false)
+  const opened = isOpen ? 'opened' : ''
+
   return (
     <Layout>
       <Header>
-        <div>
-          <h1>Kužina</h1>
-        </div>
+        <h1>Kužina</h1>
         <nav>
           <ul>
             <li>
@@ -302,6 +358,60 @@ export function Home() {
           </ul>
         </nav>
       </Header>
+
+      <HeaderMobile isMenuOpen={isOpen}>
+        <Flex>
+          <h1>Kužina</h1>
+          <ToggleMenuIcon onClick={() => setIsOpen(!isOpen)}>
+            <svg
+              aria-hidden="true"
+              focusable="false"
+              role="img"
+              viewBox="0 0 80 80"
+              className={opened}
+            >
+              <path
+                className="line line1"
+                d="M 20,29.000046 H 80.000231 C 80.000231,29.000046 94.498839,28.817352 94.532987,66.711331 94.543142,77.980673 90.966081,81.670246 85.259173,81.668997 79.552261,81.667751 75.000211,74.999942 75.000211,74.999942 L 25.000021,25.000058"
+              />
+              <path className="line line2" d="M 20,50 H 80" />
+              <path
+                className="line line3"
+                d="M 20,70.999954 H 80.000231 C 80.000231,70.999954 94.498839,71.182648 94.532987,33.288669 94.543142,22.019327 90.966081,18.329754 85.259173,18.331003 79.552261,18.332249 75.000211,25.000058 75.000211,25.000058 L 25.000021,74.999942"
+              />
+            </svg>
+          </ToggleMenuIcon>
+        </Flex>
+
+        <AnimatePresence>
+          {isOpen && (
+            <Menu
+              initial="hidden"
+              animate="show"
+              variants={menu}
+              exit={{ opacity: 0 }}
+            >
+              <ul>
+                <motion.li variants={menuItem}>
+                  <Link href="/">
+                    <a>Početna</a>
+                  </Link>
+                </motion.li>
+                <motion.li variants={menuItem}>
+                  <Link href="/recipes">
+                    <a>Recepti</a>
+                  </Link>
+                </motion.li>
+                <motion.li variants={menuItem}>
+                  <Link href="/about">
+                    <a>Što je Kužina?</a>
+                  </Link>
+                </motion.li>
+              </ul>
+            </Menu>
+          )}
+        </AnimatePresence>
+      </HeaderMobile>
 
       <Main>
         <Byline>
--- styles/global.ts
+++ styles/global.ts
@@ -80,6 +80,10 @@ export const globalStyles = css`
     width: 100%;
   }
 
+  ul {
+    list-style: none;
+  }
+
   a {
     color: inherit;
     text-decoration: none;
@@ -96,5 +100,46 @@ export const globalStyles = css`
 
   a:hover::after {
     width: 100%;
+  }
+
+  .line {
+    fill: none;
+    stroke: black;
+    stroke-width: 6;
+    transition: stroke-dasharray 600ms cubic-bezier(0.4, 0, 0.2, 1),
+      stroke-dashoffset 600ms cubic-bezier(0.4, 0, 0.2, 1);
+  }
+
+  .line1 {
+    stroke-dasharray: 60 207;
+    stroke-width: 6;
+  }
+
+  .line2 {
+    stroke-dasharray: 60 60;
+    stroke-width: 6;
+  }
+
+  .line3 {
+    stroke-dasharray: 60 207;
+    stroke-width: 6;
+  }
+
+  .opened .line1 {
+    stroke-dasharray: 90 207;
+    stroke-dashoffset: -134;
+    stroke-width: 6;
+  }
+
+  .opened .line2 {
+    stroke-dasharray: 1 60;
+    stroke-dashoffset: -30;
+    stroke-width: 6;
+  }
+
+  .opened .line3 {
+    stroke-dasharray: 90 207;
+    stroke-dashoffset: -134;
+    stroke-width: 6;
   }
 `
