309642d063af2242eb6103af7241125a53230951
mattcroat <mattcroat.dev@gmail.com>
2021-03-05 16:18:21 +0100

    feat: Add page transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,36 @@
 import { Global } from '@emotion/react'
+import { motion, AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
 import { globalStyles } from '@/root/styles/global'
 
-export default function App({ Component, pageProps }: AppProps) {
+const variants = {
+  pageTransition: {
+    hidden: { opacity: 0.1 },
+    show: { opacity: 1 },
+    yeet: { opacity: 0 },
+  },
+}
+
+export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <>
       <Global styles={globalStyles} />
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Component {...pageProps} />
+      <AnimatePresence>
+        <motion.div
+          key={router.route}
+          initial="hidden"
+          animate="show"
+          exit="yeet"
+          variants={variants.pageTransition}
+        >
+          <Component {...pageProps} />
+        </motion.div>
+      </AnimatePresence>
     </>
   )
 }
