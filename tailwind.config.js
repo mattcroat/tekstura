28f2c4d5c18bfb676d47cc852be7ed18fed55372
mattcroat <mattcroat.dev@gmail.com>
2021-03-05 21:14:10 +0100

    chore: Add Tailwind colors to config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,13 @@
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    colors: {
+      gray: colors.coolGray,
+      yellow: colors.amber,
+    },
   },
   variants: {
     extend: {},
