6597ee5fba1e2e11048ef89d3c9deda2141bfacb
mattcroat <mattcroat.dev@gmail.com>
2021-03-09 20:39:20 +0100

    style: Change Home component to use Tailwind

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -38,69 +38,80 @@ const variants = {
 export function Home() {
   return (
     <Layout>
-      <main>
+      <main className="md:px-8 lg:px-16 md:pb-8 lg:pb-16 md:grid md:grid-cols-2">
         <motion.section
-          className="p-6 md:p-8 lg:p-16"
+          className="md:grid md:place-content-center md:bg-yellow-400"
           variants={variants.byline}
           initial="hidden"
           animate="show"
         >
-          <h1 className="text-2xl">Recepti, savjeti i više</h1>
-          <p className="mt-2">
-            Tekstura je namijenjena za dijeljenje izvrsne hrane sa drugima.
-          </p>
+          <div className="px-8">
+            <h1 className="text-2xl md:text-3xl">Recepti, savjeti i više</h1>
+            <p className="mt-2 ">
+              Tekstura je namijenjena za dijeljenje izvrsne hrane sa drugima.
+            </p>
+          </div>
+
+          <motion.aside
+            className="flex flex-col items-center mt-6 md:m-6 p-6 space-y-4 bg-gray-100"
+            variants={variants.newsletter}
+            initial="hidden"
+            animate="show"
+          >
+            <img
+              className="h-16 w-16 object-cover rounded-full"
+              src="/images/avatar.webp"
+              alt="Placeholder"
+            />
+            <h2 className="text-center">
+              Besplatno primajte obavijesti u sandučić
+            </h2>
+            <form className="w-full flex flex-col gap-y-4 lg:w-auto lg:flex lg:flex-row">
+              <label className="sr-only" htmlFor="email">
+                Email
+              </label>
+              <input
+                className="p-2"
+                type="email"
+                id="email"
+                placeholder="budi@zakon.com"
+              />
+              <button className="p-2 bg-yellow-400" type="submit">
+                <div className="flex items-center justify-center">
+                  <svg
+                    className="inline h-6 w-6 mx-1"
+                    xmlns="http://www.w3.org/2000/svg"
+                    fill="none"
+                    viewBox="0 0 24 24"
+                    stroke="currentColor"
+                  >
+                    <path
+                      strokeLinecap="round"
+                      strokeLinejoin="round"
+                      strokeWidth={1.2}
+                      d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                    />
+                  </svg>
+                  <span>Prijava</span>
+                </div>
+              </button>
+            </form>
+          </motion.aside>
         </motion.section>
 
-        <motion.aside
-          className="flex flex-col items-center mt-6 p-6 space-y-4 bg-gray-100"
-          variants={variants.newsletter}
-          initial="hidden"
-          animate="show"
-        >
-          <img
-            className="h-16 w-16 object-cover rounded-full"
-            src="/images/avatar.webp"
-            alt="Placeholder"
-          />
-          <h2 className="text-lg text-center">
-            Besplatno primajte obavijesti u sandučić
-          </h2>
-          <form>
-            <label className="sr-only" htmlFor="email">
-              Email
-            </label>
-            <input
-              className="p-2"
-              type="email"
-              id="email"
-              placeholder="budi@zakon.com"
-            />
-            <button className="p-2 bg-yellow-400" type="submit">
-              Prijava
-            </button>
-          </form>
-        </motion.aside>
-
         <motion.section
-          className="relative h-screen text-center"
+          className="relative text-center"
           variants={variants.recipe}
           initial="hidden"
           animate="show"
         >
-          <img
-            className="h-full w-full object-cover"
-            src="/images/dish.webp"
-            alt="Dish"
-          />
+          <img src="/images/dish.webp" alt="Dish" />
           <div className="h-full w-full absolute top-0 flex flex-col justify-center items-center px-2 text-gray-50 z-10">
-            <h3 className="text-xl">
+            <h3 className="text-xl md:text-3xl md:px-16">
               Svako Jutro Jedno Jaje Organizmu Snagu Daje
             </h3>
             <Link href="/recepti/nevjerojatni-recept">
-              <a
-                className="mt-2 font-heading text-xl text-yellow-400 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 transition
-              "
-              >
+              <a className="mt-2 font-heading text-xl text-yellow-400 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 transition">
                 recept
               </a>
             </Link>
