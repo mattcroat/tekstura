cb8281bfb6e35584e45847218edb5bd5df09ffcf
mattcroat <mattcroat.dev@gmail.com>
2021-03-15 16:56:47 +0100

    style: Redesign landing page

--- components/Home/Home.tsx
+++ components/Home/Home.tsx
@@ -14,16 +14,6 @@ const variants = {
       },
     },
   },
-  newsletter: {
-    hidden: { opacity: 0, scale: 0 },
-    show: {
-      opacity: 1,
-      scale: 1,
-      transition: {
-        delay: 0.6,
-      },
-    },
-  },
   recipe: {
     hidden: { opacity: 0 },
     show: {
@@ -38,96 +28,89 @@ const variants = {
 export function Home() {
   return (
     <Layout>
-      <main className="md:h-screen">
-        <div className="md:h-5/6 md:flex md:p-16">
-          <motion.section
-            className="md:flex-1 md:grid md:place-content-center md:bg-yellow-400"
-            variants={variants.byline}
-            initial="hidden"
-            animate="show"
-          >
-            <div className="mt-8 md:m-0 px-8 dark:text-gray-50">
-              <h1 className="text-2xl md:text-3xl md:text-gray-800">
-                Recepti, savjeti i više
-              </h1>
-              <p className="mt-2 md:text-gray-800">
-                Tekstura je namijenjena za dijeljenje izvrsne hrane sa drugima.
-              </p>
-            </div>
+      <main className="h-screen flex flex-col mt-8 md:mt-16 md:px-16">
+        <motion.section
+          className="h-full relative text-center lg:h-1/4"
+          variants={variants.byline}
+          initial="hidden"
+          animate="show"
+        >
+          <div className="px-8 text-left dark:text-gray-50 md:px-0 md:text-center">
+            <h1 className="text-2xl md:text-3xl md:text-gray-800">
+              Recepti, savjeti i više
+            </h1>
+            <p className="mt-2 md:text-gray-800">
+              Tekstura je namijenjena za dijeljenje izvrsne hrane sa drugima.
+            </p>
+          </div>
 
-            <motion.aside
-              className="flex flex-col items-center mt-8 md:m-8 p-8 space-y-4 bg-gray-50 shadow-sm"
-              variants={variants.newsletter}
-              initial="hidden"
-              animate="show"
-            >
-              <img
-                className="h-16 w-16 object-cover rounded-full"
-                src="/images/avatar.webp"
-                alt="Placeholder"
-              />
-              <h2 className="text-center">
-                Besplatno primajte obavijesti u sandučić
-              </h2>
-              <form className="w-full md:w-auto">
-                <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:border md:border-opacity-10 shadow-sm">
-                  <label className="sr-only" htmlFor="email">
-                    Email
-                  </label>
-                  <input
-                    className="p-2 bg-white border border-opacity-10 md:border-none"
-                    type="email"
-                    id="email"
-                    placeholder="budi@zakon.com"
-                  />
-                  <button className="p-2 bg-yellow-400 shadow-sm" type="submit">
-                    <div className="flex items-center justify-center">
-                      <svg
-                        className="inline h-6 w-6 mx-1"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={1.2}
-                          d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
-                        />
-                      </svg>
-                      <span>Prijava</span>
-                    </div>
-                  </button>
-                </div>
-              </form>
-            </motion.aside>
-          </motion.section>
-
-          <motion.section
-            className="h-96 md:h-auto md:flex-1 relative text-center"
-            variants={variants.recipe}
-            initial="hidden"
-            animate="show"
-          >
+          <aside className="flex flex-col items-center mt-8 p-8 space-y-4 bg-gray-50 lg:mt-0 md:border md:border-opacity-10 md:shadow-sm lg:z-10 lg:absolute lg:bottom-0 lg:left-1/2 lg:transform lg:-translate-x-1/2 lg:translate-y-1/2">
             <img
-              className="h-full w-full object-cover"
-              src="/images/dish.webp"
-              alt="Dish"
+              className="h-16 w-16 object-cover rounded-full"
+              src="/images/avatar.webp"
+              alt="Placeholder"
             />
-            <div className="h-full w-full absolute top-0 flex flex-col justify-center items-center px-2 text-gray-50 z-10">
-              <h3 className="text-xl md:text-3xl md:px-16">
-                Svako Jutro Jedno Jaje Organizmu Snagu Daje
-              </h3>
-              <Link href="/recepti/nevjerojatni-recept">
-                <a className="mt-2 font-heading text-xl text-yellow-400 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 transition">
-                  recept
-                </a>
-              </Link>
-            </div>
-            <div className="absolute inset-0 bg-gradient-to-t from-gray-900"></div>
-          </motion.section>
-        </div>
+            <h2 className="text-center">
+              Besplatno primajte obavijesti u sandučić
+            </h2>
+            <form className="w-full md:w-auto">
+              <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 md:border md:border-opacity-10 shadow-sm">
+                <label className="sr-only" htmlFor="email">
+                  Email
+                </label>
+                <input
+                  className="p-2 bg-white border border-opacity-10 md:border-none"
+                  type="email"
+                  id="email"
+                  placeholder="budi@zakon.com"
+                />
+                <button className="p-2 bg-yellow-400 shadow-sm" type="submit">
+                  <div className="flex items-center justify-center">
+                    <svg
+                      className="inline h-6 w-6 mx-1"
+                      xmlns="http://www.w3.org/2000/svg"
+                      fill="none"
+                      viewBox="0 0 24 24"
+                      stroke="currentColor"
+                    >
+                      <path
+                        strokeLinecap="round"
+                        strokeLinejoin="round"
+                        strokeWidth={1.2}
+                        d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z"
+                      />
+                    </svg>
+                    <span>Prijava</span>
+                  </div>
+                </button>
+              </div>
+            </form>
+          </aside>
+        </motion.section>
+
+        <motion.section
+          className="h-1/2 relative text-center"
+          variants={variants.recipe}
+          initial="hidden"
+          animate="show"
+        >
+          <img
+            className="h-full w-full object-cover"
+            src="/images/dish.webp"
+            alt="Dish"
+          />
+          <div className="h-full w-full absolute top-0 flex flex-col justify-center items-center px-2 text-gray-50 z-10">
+            <h3 className="text-xl md:text-3xl md:px-16">
+              Svako Jutro Jedno Jaje Organizmu Snagu Daje
+            </h3>
+            <Link href="/recepti/nevjerojatni-recept">
+              <a className="mt-2 font-heading text-xl text-yellow-400 border-b-2 border-yellow-400 border-opacity-0 hover:border-opacity-100 transition">
+                recept
+              </a>
+            </Link>
+          </div>
+          <div className="absolute inset-0 bg-gradient-to-t from-gray-900"></div>
+        </motion.section>
       </main>
     </Layout>
   )
