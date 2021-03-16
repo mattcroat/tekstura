e60069483956f5f413d05e308fdbab00ac76dcf2
mattcroat <mattcroat.dev@gmail.com>
2021-03-16 22:38:08 +0100

    style: Remove page exit transition

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,36 +1,15 @@
-import { motion, AnimatePresence } from 'framer-motion'
 import type { AppProps } from 'next/app'
 import Head from 'next/head'
 
-import { Header } from '@/root/components/Header'
 import '@/root/styles/global.css'
 
-const variants = {
-  pageTransition: {
-    hidden: { opacity: 0.1 },
-    show: { opacity: 1 },
-    yeet: { opacity: 0 },
-  },
-}
-
-export default function App({ Component, pageProps, router }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
       <Head>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
-      <Header />
-      <AnimatePresence>
-        <motion.div
-          key={router.route}
-          initial="hidden"
-          animate="show"
-          exit="yeet"
-          variants={variants.pageTransition}
-        >
-          <Component {...pageProps} />
-        </motion.div>
-      </AnimatePresence>
+      <Component {...pageProps} />
     </>
   )
 }
