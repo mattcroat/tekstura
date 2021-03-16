844de6d65c0ee73b16b085f5c8810303089ee295
mattcroat <mattcroat.dev@gmail.com>
2021-03-16 22:39:14 +0100

    refactor: Add page transition to Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,7 +1,30 @@
+import { Header } from '@/root/components/Header'
+import { motion } from 'framer-motion'
+
 type Props = {
   children: React.ReactNode
 }
 
+const variants = {
+  pageTransition: {
+    hidden: { opacity: 0 },
+    show: { opacity: 1 },
+  },
+}
+
 export function Layout({ children }: Props) {
-  return <div className="xl:max-w-7xl xl:mx-auto">{children}</div>
+  return (
+    <>
+      <div className="xl:max-w-7xl xl:mx-auto">
+        <Header />
+        <motion.div
+          initial="hidden"
+          animate="show"
+          variants={variants.pageTransition}
+        >
+          {children}
+        </motion.div>
+      </div>
+    </>
+  )
 }
