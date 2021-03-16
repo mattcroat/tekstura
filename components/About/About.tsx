6f8c5f2c7172eb6b1f1681bce0792526e2157fd4
mattcroat <mattcroat.dev@gmail.com>
2021-03-16 22:41:08 +0100

    style: Make About page nicer

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -1,21 +1,17 @@
 import React from 'react'
-import { motion } from 'framer-motion'
-
 import { Layout } from '@/root/components/Layout'
 
 export function About() {
   return (
     <Layout>
-      <main className="mt-8 md:m-16">
-        <section className="mt-8 p-8 space-y-8 lg:w-1/2 lg:mx-auto dark:text-gray-50 md:border border-gray-200 dark:border-gray-700 shadow-sm transition">
+      <main className="mt-8 md:m-16 bg-yellow-300">
+        <section className="mt-8 p-8 space-y-8 lg:w-1/2 lg:mx-auto">
           <img
-            className="h-16 w-16 mx-auto object-cover rounded-full"
+            className="h-24 w-24 mx-auto object-cover rounded-full"
             src="/images/avatar.webp"
             alt="Placeholder"
           />
-          <h1 className="text-2xl md:text-3xl text-yellow-400 text-center ">
-            Što je Tekstura?
-          </h1>
+          <h1 className="text-2xl text-center md:text-3xl">Što je Tekstura?</h1>
           <p>
             Distinctio amet, rerum, accusantium saepe quam, ducimus possimus
             sint libero odio dolore assumenda. Dolores quibusdam nobis, quidem
