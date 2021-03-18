edbcbf1c2bdf3b5095ff63273310e157054c489b
mattcroat <mattcroat.dev@gmail.com>
2021-03-18 21:57:07 +0100

    style: Remove page fade in animation from Layout

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,16 +1,8 @@
 import { Header } from '@/root/components/Header'
-import { motion } from 'framer-motion'
 
 type LayoutProps = {
   children: React.ReactNode
   layout?: { landingPage: boolean }
-}
-
-const variants = {
-  pageTransition: {
-    hidden: { opacity: 0 },
-    show: { opacity: 1 },
-  },
 }
 
 export function Layout({ children, layout }: LayoutProps) {
@@ -27,13 +19,7 @@ export function Layout({ children, layout }: LayoutProps) {
     <>
       <div className="xl:max-w-7xl xl:mx-auto">
         <Header />
-        <motion.div
-          initial="hidden"
-          animate="show"
-          variants={variants.pageTransition}
-        >
-          {children}
-        </motion.div>
+        {children}
       </div>
     </>
   )
