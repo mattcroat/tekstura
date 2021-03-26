f03cdda968ef0f89b5d86e91b155322a7d1ca4f1
mattcroat <mattcroat.dev@gmail.com>
2021-03-26 19:58:10 +0100

    refactor: Remove author from schema

--- studio/schemas/documents/recipe.ts
+++ studio/schemas/documents/recipe.ts
@@ -86,11 +86,5 @@ export default {
       author: 'author.name',
       media: 'mainImage',
     },
-    prepare(selection) {
-      const { author } = selection
-      return Object.assign({}, selection, {
-        subtitle: author && `by ${author}`,
-      })
-    },
   },
 }
