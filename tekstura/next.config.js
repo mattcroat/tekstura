6a3841a8a83d1ce07a0b1b0bbb9a6bc901755a5b
mattcroat <mattcroat.dev@gmail.com>
2021-04-21 21:27:02 +0200

    refactor: Separate Tekstura, and Sanity frontend

--- tekstura/next.config.js
+++ tekstura/next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://localhost:3333/studio/:path*'
-            : '/studio/index.html',
+            : 'https://tekstura.sanity.studio',
       },
     ]
   },
