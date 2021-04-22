eabbc26c788448030112413bef351ff1965d5ed2
mattcroat <mattcroat.dev@gmail.com>
2021-04-22 18:24:35 +0200

    chore: Update Next.js config rewrites for Sanity Studio

--- tekstura/next.config.js
+++ tekstura/next.config.js
@@ -6,7 +6,7 @@ module.exports = {
         destination:
           process.env.NODE_ENV === 'development'
             ? 'http://localhost:3333/studio/:path*'
-            : 'https://tekstura.sanity.studio',
+            : '/studio/index.html',
       },
     ]
   },
