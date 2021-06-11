67ff9a2509e4c35aaf51956b9e5bd4105ced327a
mattcroat <mattcroat.dev@gmail.com>
2021-06-11 16:39:35 +0200

    feat: Add translations to list of views

--- sanity/deskStructure/deskStructure.ts
+++ sanity/deskStructure/deskStructure.ts
@@ -1,5 +1,6 @@
-import S from '@sanity/desk-tool/structure-builder'
 import { FaFileAlt } from 'react-icons/fa'
+import * as Structure from 'sanity-plugin-intl-input/lib/structure'
+import S from '@sanity/desk-tool/structure-builder'
 
 import about from './about'
 import home from './home'
@@ -8,28 +9,39 @@ import settings from './settings'
 import { RecipePreview } from '../components/RecipePreview'
 import { hiddenDocumentTypes } from '../utils'
 
-// adds live preview tab to any recipe
 export function getDefaultDocumentNode({ schemaType }) {
+  const [, translations] = Structure.getDocumentNodeViewsForSchemaType(
+    schemaType
+  )
+
   if (schemaType === 'recipe') {
     return S.document().views([
+      // default form for editing a document
       S.view.form(),
+      // preview
       S.view.component(RecipePreview).title('Preview'),
+      // translations
+      translations,
     ])
   }
+
+  return S.document()
 }
 
-// left sidebar in studio
 export default () =>
   S.list()
+    // left side pane
     .title('Options')
     .items([
       ...S.documentTypeListItems().filter((document: any) =>
         hiddenDocumentTypes(document, { document: true })
       ),
+      // pages category
       S.listItem()
         .icon(FaFileAlt)
         .title('Pages')
         .child(S.list().title('Pages').items([home, about])),
       S.divider(),
+      // settings category
       settings,
     ])
