e83e96c97251326b9b1aea8243259e85326b07fc
mattcroat <mattcroat.dev@gmail.com>
2021-06-29 23:00:13 +0200

    refactor: Add 404 page translation

--- tekstura/pages/404.tsx
+++ tekstura/pages/404.tsx
@@ -10,10 +10,13 @@ export default function NotFound() {
     setTimeout(() => router.replace('/'), 4000)
   }, [router])
 
+  const title =
+    router.locale === 'hr' ? '404: Stranica ne postoji' : '404: Page not found'
+
   return (
     <Layout>
       <div className="px-8 mt-8 text-center md:px-0 md:mt-16 lg:px-16 dark:text-white">
-        <h1 className="text-2xl md:text-3xl">404: Stranica ne postoji</h1>
+        <h1 className="text-2xl md:text-3xl">{title}</h1>
         <img
           className="mt-8 md:mx-auto max-h-96"
           src="/images/404.svg"
