d1bc10da5db1f75e06e2d9bcb922d770cb06985d
mattcroat <mattcroat.dev@gmail.com>
2021-08-13 13:58:34 +0200

    feat: Add logo

--- tekstura/components/Header/Navbar/Navbar.tsx
+++ tekstura/components/Header/Navbar/Navbar.tsx
@@ -16,7 +16,18 @@ export function Navbar({ translatedText }: NavbarProps) {
     <header className="hidden px-16 mt-16 dark:text-gray-50 md:flex md:justify-between md:items-baseline print:hidden">
       <div className="text-2xl font-bold font-heading md:text-3xl">
         <Link href="/">
-          <a>{translatedText?.title}</a>
+          <a className="flex items-center gap-2">
+            <svg
+              className="w-8 h-8 text-gold"
+              viewBox="0 0 144 182"
+              fill="currentColor"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path d="M78.186 168.907c0 8.718-4.29 13.407-5.939 13.077-1.65.33-5.938-4.359-5.938-13.077V95.598c.33-1.816-.89-6.736-8.412-11.888-9.402-6.439-19.794-17.831-19.794-38.635S51.463 0 72.247 0s34.145 24.271 34.145 45.075c0 20.804-10.392 32.196-19.794 38.635-7.522 5.152-8.742 10.072-8.412 11.888v73.309z" />
+              <path d="M89.072 104.019v41.608h20.784v-41.608C121.072 103.028 144 93.815 144 72.813c0-26.252-16.208-35.663-27.216-35.663C121.237 71.327 99 88.5 93.031 91.636c-3.471 1.823-3.794 9.411-3.959 12.383zM56 104.019v41.608H35.217v-41.608C24 103.028 1.072 93.815 1.072 72.813c0-26.252 16.208-35.663 27.217-35.663C23.835 71.327 46.072 88.5 52.04 91.636c3.471 1.823 3.794 9.411 3.959 12.383z" />
+            </svg>
+            <span>{translatedText?.title}</span>
+          </a>
         </Link>
       </div>
 
